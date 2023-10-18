c364beb7014069501cbcd662c4f55e6255329ac6
Tomi Belan <tomi.belan@gmail.com>
2023-10-18 03:29:16 +0200

    sshfs: Fix file permissions. Document used sshfs options.

--- sshfs/start_sshfs2.ps1
+++ sshfs/start_sshfs2.ps1
@@ -13,24 +13,76 @@ $Env:CYGWIN += " wincmdln"  # Just so that kill_ssh_now.ps1 can detect it. https
 $exe = "$PSScriptRoot\build_output\bin\sshfs.exe"
 
 $argarr = @(
+  # SSHFS option. Stay in the foreground. This script will take care of running it in background depending on -Waitless.
   "-f",
-  "-ofstypename=SSHFS",  # sshfs-win.c does it, unclear why
+
+  # WinFsp option. Copied from sshfs-win.c. I don't know what exactly does it do.
+  "-ofstypename=SSHFS",
+
+  # WinFsp option. Set the hidden attribute on dotfiles. It looks nice.
+  "-odothidden",
+
+  # WinFsp option. Ignore any st_uid and st_gid received from the FUSE ops.getattr/ops.fgetattr implementation.
+  # Pretend that all files are owned by the current user and group. See src/dll/fuse/fuse_intf.c.
+  # In case of SSHFS, most server-side users/groups probably don't have an equivalent local user/group.
+  # They would be mapped to a nonexistent NT user, or collide with an unrelated one. It's usually fine, but this is more correct.
+  # (You can try it without this option by running "icacls X:\path\file" to print the ACL of various files.)
+  "-ouid=-1,gid=-1",
+
+  # WinFsp option. Ignore the permission bits of any st_mode received from the FUSE ops.getattr/ops.fgetattr implementation.
+  # Pretend that all files have mode 700. See src/dll/fuse/fuse_intf.c.
+  # WinFsp should just do the syscall and let SSHFS (or rather, the remote sftp-server) decide whether to allow it.
+  # Especially with "-ouid=-1,gid=-1", who knows which of [user, group, other] bits should it even look at.
+  # Linux FUSE has default_permissions off and allow_other off by default. WinFsp doesn't implement those options. This emulates them.
+  "-oumask=077",
+
+  # WinFsp option. Ignore the PSECURITY_DESCRIPTOR received from Windows.
+  # Instead, always call ops.create() with mode 644 and ops.mkdir() with mode 755. See src/dll/fuse/fuse_intf.c.
+  # Without this, most ways to create a file or directory (e.g. Explorer, MSYS2, cmd.exe mkdir) resulted in mode 700.
+  # ops.mknod() is not completely overridden by this, but it's only called in rare circumstances.
+  # ops.chmod() is not overridden by this, but there's nothing I can do about that.
+  "-ocreate_file_umask=0133",
+  "-ocreate_dir_umask=022",
+
+  # SSHFS option. Map the current user's remote uid/gid to local uid/gid in ops.getattr, and local to remote in ops.chown.
+  # The getattr IDs are ignored anyway because of -ouid=-1,gid=-1, but best effort handling of chown might be nice, I guess.
+  "-oidmap=user",
+
+  # SSHFS option. The ssh program to use.
   "-ossh_command=/usr/bin/ssh",
-#  "-ouid=-1,gid=-1",  # probably not needed
-  "-oidmap=user",  # Needed in order to access e.g. folders with mode 700. (Does winfsp always enable "default_permissions"??)
+
+  # SSHFS option. Don't start ssh immediately, wait until it's needed.
   "-odelay_connect",
+
+  # SSHFS option. Reconnect if ssh dies or disconnects. With delay_connect, wait until it's needed.
   "-oreconnect",
+
+  # SSH option. The known hosts file.
   "-oUserKnownHostsFile=$scriptrootslash/known_hosts",
+
+  # SSH option. The private key file.
   "-oIdentityFile=$scriptrootslash/tomi-REMOVED-id_ed25519",
+
+  # SSH option. Use only public key authentication.
   "-oPreferredAuthentications=publickey",
-  "-oGlobalKnownHostsFile=sshfs_ssh_marker",  # Set a not too useful option to a nonexistent file. Used by kill_ssh_now.ps1.
+
+  # SSH option. Set an otherwise unneeded option to a nonexistent file. Used by kill_ssh_now.ps1 to identify the process.
+  "-oGlobalKnownHostsFile=sshfs_ssh_marker",
+
+  # WinFsp option. It should hopefully mount it as a network drive instead of a fixed drive.
+  # For example in rclone, AFAICT the only effect of the --network-mode option is to enable the --VolumePrefix option.
+  # https://rclone.org/commands/rclone_mount/#mounting-modes-on-windows
   "--VolumePrefix=/mysshfs/REMOVED",
+
+  # Custom SSHFS option. Set the list of top-level directories and which paths they map to.
   "-osubwrap_paths=myhome=/home/tomi:root=/",
+
   "tomi@REMOVED:/",
   "S:"  # no trailing comma
 )
 
 if ($Verbose) {
+  # SSHFS option. Enable debug logs.
   $argarr = @("-d") + $argarr
 }
 
